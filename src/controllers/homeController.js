501476f95b5bdb789695c16cf37f3ae2e0bcc79d
Trinh Tram Anh <tramanh.912.pch@gmail.com>
2023-03-12 22:30:23 +0700

    tovideo#25

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,4 +1,5 @@
 import db from'../models/index';
+import CRUDService from '../services/CRUDService';
 
 
 let getHomePage = async(req, res) =>{
@@ -20,12 +21,25 @@ let getAboutPage =(req,res) =>{
     return res.render("test/about.ejs")
 }
 
+let getCRUD =(req,res) =>{
+    return res.render("crud.ejs");
+}
+
+let postCRUD = async(req,res) =>{
+    let message = await CRUDService.createNewUser(req.body);
+    console.log(message);
+    return res.send('post crud from server');
+}
+
+
 // object:{
 //     key:''
 //     value:''
 // }
-
+ 
 module.exports={
     getHomePage: getHomePage,
     getAboutPage: getAboutPage,
+    getCRUD: getCRUD,
+    postCRUD: postCRUD,
 }
--- src/route/web.js
+++ src/route/web.js
@@ -6,9 +6,12 @@ let router = express.Router();
 let initWebRoutes = (app) => {
 
     router.get('/', homeController.getHomePage);
-
     //rest api
     router.get('/about' , homeController.getAboutPage);
+    router.get('/crud' , homeController.getCRUD);
+
+    router.post('/post-crud' , homeController.postCRUD);
+
 
     return app.use("/",router);
 }
